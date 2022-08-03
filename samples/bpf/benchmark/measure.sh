195694758b400b754df4664977a51f70a9d41286
wtj <zhangwt1997@gmail.com>
2022-08-03 12:06:46 +0800

    benchmark: add script for measurement

---
+++ samples/bpf/benchmark/measure.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+prog_num=$( cat program_list.txt | wc -l )
+
+for prog in $( cat program_list.txt )
+do
+    echo $prog
+    ./user $prog
+done
+
+echo "/////////// Summary ///////////"
+dmesg | grep 't_delta' | tail -n $prog_num | cut -c 25-
