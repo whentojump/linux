cf86c33d97ca164e120058828155e116e9138383
wtj <zhangwt1997@gmail.com>
2022-08-03 18:36:06 +0800

    benchmark: update measure script

--- samples/bpf/benchmark/measure.sh
+++ samples/bpf/benchmark/measure.sh
@@ -8,5 +8,34 @@ do
     ./user autogen/$prog
 done
 
-echo "/////////// Summary ///////////"
-dmesg | grep 't_delta' | tail -n $prog_num | cut -c 25-
+echo
+echo "//////////////// Summary ///////////////"
+echo
+echo "Nominal (estimated) program size"
+echo "--------------------------------"
+
+for prog in $( cat program_list.txt )
+do
+    sed -e 's/.o//' -e 's/kern_//' <<< $prog
+done
+
+echo
+echo "Real program size"
+echo "-----------------"
+
+for prog in $( cat program_list.txt )
+do
+    llvm-objdump -d  autogen/$prog                                            |\
+        tail     -n  1                                                        |\
+        cut      -d ':' -f 1                                                  |\
+        tr       -d ' '
+done
+
+echo
+echo "Time"
+echo "----"
+
+dmesg                                                                         |\
+    grep   't_delta'                                                          |\
+    tail -n $prog_num                                                         |\
+    cut  -c 25-
