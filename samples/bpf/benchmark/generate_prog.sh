983386c828ef8dc9f7855bc667d72e71d043bfd9
wtj <zhangwt1997@gmail.com>
2022-08-03 20:30:37 +0800

    benchmark: actually generate the programs

--- samples/bpf/benchmark/generate_prog.sh
+++ samples/bpf/benchmark/generate_prog.sh
@@ -3,6 +3,13 @@
 base_prog_src_filename=$1
 base_workload_src_filename=$2
 
+rm autogen/workload_1k.c
+
+for i in $(seq 1000)
+do
+    cat $base_workload_src_filename >> autogen/workload_1k.c
+done
+
 for prog in $( cat program_list.txt )
 do
     # Get the source filename
@@ -12,6 +19,8 @@ do
     bpf_asm_kilo_insns=$( sed -e 's/^kern_//' -e 's/k.o$//' <<< $prog )
     bpf_asm_insns=$( bc <<< "1000 * $bpf_asm_kilo_insns" )
     # TODO estimate $c_insns from $bpf_asm_insns
+    c_insns=$bpf_asm_insns
+    c_kilo_insns=$( bc <<< "$c_insns / 1000" )
 
     # Generate source files
     echo "Generating autogen/$src"
@@ -21,5 +30,10 @@ do
     sed -i "s/workload_base/workload_${bpf_asm_kilo_insns}k/" autogen/$src
     # TODO Temporarily cheat this, just in order to compile
     echo "Generating autogen/workload_${bpf_asm_kilo_insns}k.c"
-    cp $base_workload_src_filename autogen/workload_${bpf_asm_kilo_insns}k.c
+    for i in $(seq $c_kilo_insns)
+    do
+        cat autogen/workload_1k.c >> autogen/workload_${bpf_asm_kilo_insns}k.c
+    done
 done
+
+rm autogen/workload_1k.c
