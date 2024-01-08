314baed6715c614d35cd0a2f887042a7d9217f1a
wtj <zhangwt1997@gmail.com>
2024-01-07 23:09:02 -0500

    port some types

--- kernel/gcov/scc.h
+++ kernel/gcov/scc.h
@@ -21,16 +21,17 @@ enum ValueKind {
 
 typedef void *IntPtrT;
 
+// The actual size is 60B, aligned to 64B.
 typedef struct __llvm_profile_data {
 	const uint64_t NameRef;
 	const uint64_t FuncHash;
 	const IntPtrT CounterPtr;
-	const IntPtrT BitmapPtr;
+	const IntPtrT BitmapPtr;                     // added by the MC/DC patch
 	const IntPtrT FunctionPointer;
 	IntPtrT Values;
 	const uint32_t NumCounters;
-	const uint16_t NumValueSites[IPVK_Last+1];
-	const uint32_t NumBitmapBytes;
+	const uint16_t NumValueSites[IPVK_Last+1];   // 2-elem array
+	const uint32_t NumBitmapBytes;               // added by the MC/DC patch
 } __llvm_profile_data;
 
 // compiler-rt/lib/profile/InstrProfilingInternal.c
